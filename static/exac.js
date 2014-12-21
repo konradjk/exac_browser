a373c53e7fefddf5dd00fb6fe4170001a5656353
konradjk <konradjkarczewski@gmail.com>
2014-12-21 15:26:31 -0500

    Export to SVG for both coverage plot and exon

--- static/exac.js
+++ static/exac.js
@@ -634,11 +634,24 @@ function date_format(date) {
             pad_2(date.getSeconds()) ;
 }
 
-function set_plot_image(container) {
-    var $container = $('#' + container);
-    var content = $container.html().trim();
-    var canvas = document.getElementById(container + '_canvas');
-    canvg(canvas, content);
-    var theImage = canvas.toDataURL('image/png');
-    $('#' + container + '_png').attr('href', theImage);
+function set_plot_image(container, index) {
+    //get svg element.
+    var svg = $('#' + container).find('svg')[index];
+    //get svg source.
+    var serializer = new XMLSerializer();
+    var source = serializer.serializeToString(svg);
+
+    //add name spaces.
+    if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+    if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+    }
+
+    //add xml declaration
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+    //convert svg source to URI data scheme.
+    return "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
 }
