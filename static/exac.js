d520b57756e6bfe48fd8d5aa84786cb21cd8eb50
Matthew Solomonson <ggmatt@gmail.com>
2016-09-23 11:49:15 -0400

    add snp indel filter

--- static/exac.js
+++ static/exac.js
@@ -554,16 +554,81 @@ function draw_region_coverage(raw_data, metric, ref) {
     }
 }
 
+var csq_order = [
+  'transcript_ablation',
+  'splice_acceptor_variant',
+  'splice_donor_variant',
+  'stop_gained',
+  'frameshift_variant',
+  'stop_lost',
+  'start_lost',
+  'inframe_insertion',
+  'inframe_deletion',
+  'missense_variant',
+  'protein_altering_variant',
+  'incomplete_terminal_codon_variant',
+  'stop_retained_variant',
+  'synonymous_variant',
+  'coding_sequence_variant',
+  'mature_miRNA_variant',
+  '5_prime_UTR_variant',
+  '3_prime_UTR_variant',
+  'non_coding_transcript_exon_variant',
+  'non_coding_exon_variant',
+  'NMD_transcript_variant',
+  'non_coding_transcript_variant',
+  'nc_transcript_variant',
+  'downstream_gene_variant',
+  'TFBS_ablation',
+  'TFBS_amplification',
+  'TF_binding_site_variant',
+  'regulatory_region_ablation',
+  'regulatory_region_amplification',
+  'feature_elongation',
+  'regulatory_region_variant',
+  'feature_truncation',
+  'intergenic_variant',
+  ''
+]
+
+var categoryDefinitions = {
+  all: csq_order,
+  lof: csq_order.slice(0, csq_order.indexOf('frameshift_variant')),
+  missense: csq_order.slice(0, csq_order.indexOf('synonymous_variant')),
+}
+
+function uniqueMajorConsequences(variants_in_gene) {
+  return _.uniq(variants_in_gene.map(function(variant) {
+    return variant.major_consequence
+  }))
+}
+
 function update_variants() {
-    var category = $('.consequence_display_buttons.active').attr('id').replace('consequence_', '').replace('_variant_button', '');
-    var filter = $('#filtered_checkbox').is(":checked") ? '[filter_status]' : '[filter_status="PASS"]';
-    $('[category]').hide();
-    if (category == 'other') {
-        $('[category]' + filter).show();
-    } else if (category == 'missense') {
-        $('[category=missense_variant]' + filter).show();
-    }
-    $('[category=lof_variant]' + filter).show();
+    var category = $('.consequence_display_buttons.active')
+        .attr('id')
+        .replace('consequence_', '')
+        .replace('_variant_button', '');
+    var filterState = $('#filtered_checkbox').is(":checked")
+    var indelState = $('.indel_display_buttons.active')
+        .attr('id')
+        .replace('indel_selection_', '')
+        .replace('_button', '');
+    $('[major_consequence]').hide()
+    $('[major_consequence]').map(function(row) {
+        if (!filterState && $(this).attr('filter_status') !== 'PASS') {
+            return
+        }
+        if (indelState === 'snp' && $(this).attr('indel') === 'true') {
+            return
+        }
+        if (indelState === 'indel' && $(this).attr('indel') === 'false') {
+            return
+        }
+        if (_.contains(categoryDefinitions[category], $(this).attr('major_consequence'))
+        ) {
+            $(this).show()
+        }
+    })
     if ($('tr[style!="display: none;"]').length == 1) {
         $('#variants_table_empty').show();
         $('#variants_table_container').hide();
