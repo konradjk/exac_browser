b17cc7bd0b0795d13061979b8cd8d3dbeda62a1e
Huy Hong <huyhong@gmail.com>
2016-10-18 18:36:33 -0700

    Add gray dash into empty variant_table cells

--- static/custom.js
+++ static/custom.js
@@ -19,12 +19,18 @@ $(document).ready(function() {
     }
   })
 
-  // Make .table_variant row clickable to the first link found
   // Slight delay is needed because table is dynamically generated, should fix so these events bind once the tables generated
   $("body").delay(500).queue(function() {
+    // Make .table_variant row clickable to the first link found
     $("#variant_table .table_variant").click(function() {
       var link = $(this).find("a").attr('href')
       window.location = link
+    })
+    // add gray em dash to empty table cells
+    $("#variant_table td").each(function(index) {
+      if($(this).text() == "") {
+        $(this).append("<span class='empty-cell'></span>")
+      }
     })
   })
 })
